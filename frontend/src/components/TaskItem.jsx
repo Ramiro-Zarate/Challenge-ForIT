eee71f2b734901bf6c679ae8a3914c57034c51e1
Ramiro-Zarate <ramazara13@gmail.com>
2026-05-07 18:45:45 -0300

    feat: TaskList mock

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,15 +1,9 @@
-import styles from './TaskForm.module.css'
+import styles from './TaskItem.module.css'
 // Muestra cada tarea individualmente
 export function TaskItem() {
     return (
         <>
-            <form role='search'>
-                <input 
-                    className={styles.searchInput}
-                    type="text" 
-                    placeholder="Buscar tarea" 
-                />
-            </form>
+            
         </>
     )
 }
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,8 +1,22 @@
-import styles from './TaskForm.module.css'
+import styles from './TaskList.module.css'
 // Muestra la lista de tareas
 export function TaskList() {
     return (
         <>
+        <section className={styles.taskListSection}>
+            <div>
+                <form role='search' className={styles.searchForm}>
+                <input 
+                    className={styles.searchInput}
+                    type="text" 
+                    placeholder="Buscar tarea" 
+                />
+            </form>
+            </div>
+            <div>
+
+            </div>
+        </section>
         </>
     )
 }
