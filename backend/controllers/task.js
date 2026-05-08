092ef68c47dcd6fd2fa8c690335539cd985341dd
Ramiro-Zarate <ramazara13@gmail.com>
2026-05-08 09:56:44 -0300

    feat: Add task controller

---
+++ backend/controllers/task.js
@@ -0,0 +1,35 @@
+import { TaskModel } from "../models/task"
+
+export class TaskController {
+    static async getAll(req, res) {
+        const tasks = await TaskModel.getAll()
+        res.json(tasks)
+    }
+
+    static async getById(req, res) {
+        const { id } = req.params
+        const task = await TaskModel.getById({ id })
+        if (!task) {
+            return res.status(404).json({ message: 'Tarea no encontrada' })
+        }
+        res.json(task)
+    }
+
+    static async create(req, res) {
+        const newTask = await TaskModel.create({ input: req.body })
+        req.status(201).json(newTask)
+    }
+
+    static async delete(req, res) {
+        const { id } = req.params
+        const result = await TaskModel.delete({ id })
+
+        if (!result) {
+            return res.status(404).json({ message: 'Tarea no encontrada' })
+        }
+
+        return res.json({ message: 'Tarea eliminada con éxito' })
+    }
+
+
+}
